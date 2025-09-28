eb70bd460a3bf7bcff0e93f13ea589fb6efdcc1a
Miyoshi-Ryota <m1yosh1.ry0t4@gmail.com>
2025-09-29 02:29:44 +0900

    chore: Apply cargo clippy --fix

--- src/client.rs
+++ src/client.rs
@@ -405,12 +405,11 @@ impl Client {
                         )
                         .await;
 
-                    if let Ok(auth_result) = result {
-                        if auth_result.success() {
+                    if let Ok(auth_result) = result
+                        && auth_result.success() {
                             auth_success = true;
                             break;
                         }
-                    }
                 }
 
                 if !auth_success {
